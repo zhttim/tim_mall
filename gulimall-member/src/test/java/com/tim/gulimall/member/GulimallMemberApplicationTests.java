0db582c7da99943a485d399e0682977b065a16eb
Tim <zhttim1805@gmail.com>
2022-05-07 14:03:53 +0800

    项目初始化结构创建

---
+++ gulimall-member/src/test/java/com/tim/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tim.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
