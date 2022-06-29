4821af152ca71487e687ed6e7dba431ffe8212b8
Tim <zhttim1805@gmail.com>
2022-06-29 18:30:43 +0800

    树形展示三级分类数据

--- renren-fast-vue/src/views/modules/product/category.vue
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -1,7 +1,7 @@
 <!--  -->
 <template>
   <el-tree
-    :data="data"
+    :data="menus"
     :props="defaultProps"
     @node-click="handleNodeClick"
   ></el-tree>
@@ -16,10 +16,10 @@ export default {
   components: {},
   data() {
     return {
-      data: [],
+      menus: [],
       defaultProps: {
         children: "children",
-        label: "label",
+        label: "name",
       },
     };
   },
@@ -36,14 +36,15 @@ export default {
       this.$http({
         url: this.$http.adornUrl("/product/category/list/tree"),
         method: "get",
-      }).then((data) => {
-        console.log("成功获取菜单数据", data);
+      }).then(({data}) => {
+        console.log("成功获取菜单数据", data.data);
+        this.menus = data.data
       });
     },
   },
   //生命周期 - 创建完成（可以访问当前this实例）
   created() {
-      this.getMenus;
+      this.getMenus();
   },
   //生命周期 - 挂载完成（可以访问DOM元素）
   mounted() {},
