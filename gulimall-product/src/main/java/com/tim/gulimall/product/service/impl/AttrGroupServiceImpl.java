1673f6867d60aea25a4a7dc93522e25c72733262
Tim <zhttim1805@gmail.com>
2022-07-22 15:18:19 +0800

    获取分类属性分组

--- gulimall-product/src/main/java/com/tim/gulimall/product/service/impl/AttrGroupServiceImpl.java
+++ gulimall-product/src/main/java/com/tim/gulimall/product/service/impl/AttrGroupServiceImpl.java
@@ -3,6 +3,7 @@ package com.tim.gulimall.product.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.mysql.cj.util.StringUtils;
 import com.tim.common.utils.PageUtils;
 import com.tim.common.utils.Query;
 import com.tim.gulimall.product.dao.AttrGroupDao;
@@ -24,6 +25,28 @@ public class AttrGroupServiceImpl extends ServiceImpl<AttrGroupDao, AttrGroupEnt
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public PageUtils queryPage(Map<String, Object> params, Long catelogId) {
+        if (catelogId == 0) {
+            IPage<AttrGroupEntity> page = this.page(
+                    new Query<AttrGroupEntity>().getPage(params),
+                    new QueryWrapper<AttrGroupEntity>());
+            return new PageUtils(page);
+        } else {
+            String key = (String) params.get("key");
+            QueryWrapper<AttrGroupEntity> wapper = new QueryWrapper<AttrGroupEntity>().eq("catelog_id", catelogId);
+            if (!StringUtils.isNullOrEmpty(key)) {
+                wapper.and((obj) -> {
+                    obj.eq("attr_group_id", key).or().like("attr_group_name", key);
+                });
+            }
+            IPage<AttrGroupEntity> page = this.page(
+                    new Query<AttrGroupEntity>().getPage(params), wapper);
+            return new PageUtils(page);
+        }
+
     }
 
 }
