b2b4766d776e9f89eaeec8f361546f93919c9669
Tim <zhttim1805@gmail.com>
2022-08-09 16:12:08 +0800

    保存sku基本信息

--- gulimall-product/src/main/java/com/tim/gulimall/product/service/impl/SpuInfoServiceImpl.java
+++ gulimall-product/src/main/java/com/tim/gulimall/product/service/impl/SpuInfoServiceImpl.java
@@ -6,12 +6,11 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.tim.common.utils.PageUtils;
 import com.tim.common.utils.Query;
 import com.tim.gulimall.product.dao.SpuInfoDao;
-import com.tim.gulimall.product.entity.AttrEntity;
-import com.tim.gulimall.product.entity.ProductAttrValueEntity;
-import com.tim.gulimall.product.entity.SpuInfoDescEntity;
-import com.tim.gulimall.product.entity.SpuInfoEntity;
+import com.tim.gulimall.product.entity.*;
 import com.tim.gulimall.product.service.*;
 import com.tim.gulimall.product.vo.BaseAttrs;
+import com.tim.gulimall.product.vo.Images;
+import com.tim.gulimall.product.vo.Skus;
 import com.tim.gulimall.product.vo.SpuSaveVo;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,6 +37,15 @@ public class SpuInfoServiceImpl extends ServiceImpl<SpuInfoDao, SpuInfoEntity> i
 
     @Autowired
     ProductAttrValueService productAttrValueService;
+
+    @Autowired
+    SkuInfoService skuInfoService;
+
+    @Autowired
+    SkuImagesService skuImagesService;
+
+    @Autowired
+    SkuSaleAttrValueService skuSaleAttrValueService;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -86,6 +94,52 @@ public class SpuInfoServiceImpl extends ServiceImpl<SpuInfoDao, SpuInfoEntity> i
         productAttrValueService.saveProductAttr(productAttrValueEntities);
         //保存spu积分信息
         //保存spu对应sku信息
+        List<Skus> skus = spuSaveVo.getSkus();
+        if (skus != null && skus.size() > 0) {
+            skus.forEach(item -> {
+                String defaultImage = "";
+                // 遍历 skus 集合中的图片
+                for (Images image : item.getImages()) {
+                    // 默认图片等于 1 该记录则是默认图片
+                    if (image.getDefaultImg() == 1) {
+                        defaultImage = image.getImgUrl();
+                    }
+                }
+                SkuInfoEntity skuInfoEntity = new SkuInfoEntity();
+                BeanUtils.copyProperties(item, skuInfoEntity);
+                // 其他属性需要自己赋值
+                skuInfoEntity.setBrandId(spuInfoEntity.getBrandId());
+                skuInfoEntity.setCatalogId(spuInfoEntity.getCatalogId());
+                skuInfoEntity.setSaleCount(0L);
+                skuInfoEntity.setSpuId(spuInfoDescEntity.getSpuId());
+                skuInfoEntity.setSkuDefaultImg(defaultImage);
+                //保存sku基本信息
+                skuInfoService.saveSkuInfo(skuInfoEntity);
+
+                Long skuId = skuInfoEntity.getSkuId();
+
+                List<SkuImagesEntity> imagesEntities = item.getImages().stream().map(img -> {
+                    SkuImagesEntity skuImagesEntity = new SkuImagesEntity();
+                    skuImagesEntity.setSkuId(skuId);
+                    skuImagesEntity.setImgUrl(img.getImgUrl());
+                    skuImagesEntity.setDefaultImg(img.getDefaultImg());
+
+                    return skuImagesEntity;
+                }).collect(Collectors.toList());
+                //保存SKU的图片信息
+                skuImagesService.saveBatch(imagesEntities);
+
+                List<SkuSaleAttrValueEntity> skuSaleAttrValueEntities = item.getAttr().stream().map(attr -> {
+                    SkuSaleAttrValueEntity skuSaleAttrValueEntity = new SkuSaleAttrValueEntity();
+                    BeanUtils.copyProperties(attr, skuSaleAttrValueEntity);
+                    skuSaleAttrValueEntity.setSkuId(skuId);
+                    return skuSaleAttrValueEntity;
+                }).collect(Collectors.toList());
+                //保存SKU的销售属性信息
+                skuSaleAttrValueService.saveBatch(skuSaleAttrValueEntities);
+
+            });
+        }
     }
 
     @Override
